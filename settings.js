54c446576818571d9322da20f7d24dd7fe199e22
rfkhrmwn <moontonakun481@gmail.com>
2025-05-15 11:11:58 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 global.creator = 'rfkhrmwn'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://froplayer9:susi312@cluster0.pa8jyj7.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "rfk0312" //isi apa aja bebas
 global.your_email = "rifkihermawan836@gmail.com" //email
 global.email_password = "@rifki312" //application password email
